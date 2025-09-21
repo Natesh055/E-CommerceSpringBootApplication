58068493a81ce671463b4e8110b42484f8d41a4b
Natesh055 <singhnatesh50@gmail.com>
2025-09-21 12:48:33 +0530

    Established connection with mongoDB Atlas

--- src/main/java/com/nateshECommerce/EcommerceApp/EcommerceAppApplication.java
+++ src/main/java/com/nateshECommerce/EcommerceApp/EcommerceAppApplication.java
@@ -1,12 +1,18 @@
 package com.nateshECommerce.EcommerceApp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class EcommerceAppApplication {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.data.mongodb.uri", dotenv.get("MONGODB_URI"));
+        System.setProperty("spring.data.mongodb.database", dotenv.get("MONGO_DATABASE"));
 		SpringApplication.run(EcommerceAppApplication.class, args);
 	}
 
